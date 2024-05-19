04648f9d52fd0a92cd6819455a84f129cd721dee
ChaiGans <elbertchailes888@gmail.com>
2024-05-19 21:20:40 +0700

    feat: mainwindow gui done

--- Selesaikan/MainWindow.xaml.cs
+++ Selesaikan/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace Selesaikan
 {
@@ -16,9 +17,71 @@ namespace Selesaikan
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string currentActiveAlgorithm;
         public MainWindow()
         {
             InitializeComponent();
+            SetInitialImage();
+            this.currentActiveAlgorithm = "KMP";
+            UpdateButtonColors();
         }
+
+        private void SetInitialImage()
+        {
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri("pack://application:,,,/Images/insertimage.png");
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;  // Ensures the image is cached at load time
+            bitmap.EndInit();
+            displayImage.Source = bitmap;
+        }
+
+        private void btnLoadImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Select an image";
+            openFileDialog.Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                BitmapImage bitmap = new BitmapImage(new Uri(openFileDialog.FileName));
+                displayImage.Source = bitmap;
+            }
+        }
+
+        private void kmpButton_Click(object sender, RoutedEventArgs e)
+        {
+            currentActiveAlgorithm = "KMP";
+            UpdateButtonColors();
+        }
+
+        private void bmButton_Click(object sender, RoutedEventArgs e)
+        {
+            currentActiveAlgorithm = "BM";
+            UpdateButtonColors();
+        }
+
+        private void UpdateButtonColors()
+        {
+            // Reset all button backgrounds to default
+            kmpButton.Background = new SolidColorBrush(Colors.Azure);
+            bmButton.Background = new SolidColorBrush(Colors.Azure);
+            searchButton.Background = new SolidColorBrush(Colors.Azure);
+
+            // Highlight the button matching the current algorithm
+            switch (currentActiveAlgorithm)
+            {
+                case "KMP":
+                    kmpButton.Background = new SolidColorBrush(Colors.LightGreen);
+                    break;
+                case "BM":
+                    bmButton.Background = new SolidColorBrush(Colors.LightGreen);
+                    break;
+                case "Search":
+                    searchButton.Background = new SolidColorBrush(Colors.LightGreen);
+                    break;
+            }
+        }
+
     }
 }
