9b22ff82d117d9d41bb5fe3c6ed268aac43a9328
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-05-25 12:37:35 +0700

    feat: add encrypt

---
+++ Selesaikan/Utility/Encrypt.cs
@@ -0,0 +1,73 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+public class AESEncryptionExample
+{
+    private static byte[] key = Encoding.UTF8.GetBytes("0123456789ABCDEF");
+
+    public static string EncryptString(string plainText)
+    {
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = key;
+            aesAlg.GenerateIV();
+
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                
+                msEncrypt.Write(aesAlg.IV, 0, aesAlg.IV.Length);
+
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+
+                    csEncrypt.Write(plainBytes, 0, plainBytes.Length);
+                }
+
+                return Convert.ToBase64String(msEncrypt.ToArray());
+            }
+        }
+    }
+
+    public static string DecryptString(string cipherText)
+    {
+        byte[] cipherBytes = Convert.FromBase64String(cipherText);
+
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = key;
+
+            byte[] iv = new byte[aesAlg.IV.Length];
+            Array.Copy(cipherBytes, 0, iv, 0, iv.Length);
+            aesAlg.IV = iv;
+
+            ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+            using (MemoryStream msDecrypt = new MemoryStream(cipherBytes, iv.Length, cipherBytes.Length - iv.Length))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        return srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+    public static void Main(string[] args)
+    {
+        string originalText = "../../Path/Hehe.txt";
+        Console.WriteLine("Original Text: " + originalText);
+
+        string encryptedText = EncryptString(originalText);
+        Console.WriteLine("Encrypted Text: " + encryptedText);
+
+        string decryptedText = DecryptString(encryptedText);
+        Console.WriteLine("Decrypted Text: " + decryptedText);
+    }
+}
